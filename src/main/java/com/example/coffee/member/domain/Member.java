ea1f3dbcd6ba79ae1a9d7efc0a1b399327de2d99
eashhyeon3232 <jihyeon-lou3232@naver.com>
2026-07-15 17:48:48 +0900

    feat: 주문 시스템 도메인 엔티티 초기 설계 추가

---
+++ src/main/java/com/example/coffee/member/domain/Member.java
@@ -0,0 +1,22 @@
+package com.example.coffee.member.domain;
+
+import com.example.coffee.common.entity.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
---
+++ src/main/java/com/example/coffee/order/domain/OutboxStatus.java
@@ -0,0 +1,7 @@
+package com.example.coffee.order.domain;
+
+public enum OutboxStatus {
+    PENDING,
+    SENT,
+    FAILED
+}

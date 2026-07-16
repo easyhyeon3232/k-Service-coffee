739dcbe699915a3e60d653e26de24efb588522ac
eashhyeon3232 <jihyeon-lou3232@naver.com>
2026-07-16 20:17:46 +0900

    feat: 커피 주문 결제 API 구현

--- src/main/java/com/example/coffee/domin/point/repository/PointWalletRepository.java
+++ src/main/java/com/example/coffee/domin/point/repository/PointWalletRepository.java
@@ -3,8 +3,18 @@ package com.example.coffee.domin.point.repository;
 import com.example.coffee.domin.point.entity.PointWallet;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import jakarta.persistence.LockModeType;
 
 public interface PointWalletRepository extends JpaRepository<PointWallet, Long> {
 
     Optional<PointWallet> findByMemberId(Long memberId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select pointWallet from PointWallet pointWallet join fetch pointWallet.member member "
+            + "where member.id = :memberId")
+    Optional<PointWallet> findByMemberIdForUpdate(@Param("memberId") Long memberId);
 }
